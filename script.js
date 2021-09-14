332c398d58983f3e63af7d27cdffbb67b295ab2e
PontusVallgren <pontus.wallgren92@gmail.com>
2021-09-14 16:02:00 +0200

    Improved the code and added footer with a little twist

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const main = document.querySelector('.content');
 const footer = document.querySelector('.footer');
 let taskActive = 0;
 let taskDone = 0;
-const todoList = [];
+let todoList = [];
 
 // Header
 const headerText = document.createElement('h1');
@@ -18,25 +18,37 @@ header.appendChild(headerIcon);
 header.appendChild(headerText);
 
 // Input-Container
-const inputContainer = document.createElement('div');
+const inputContainer = document.createElement('form');
 inputContainer.setAttribute('id', 'input-container');
 main.appendChild(inputContainer);
 
-const inputTask = document.createElement('input');
+let inputTask = document.createElement('input');
 inputTask.setAttribute('id', 'input-task');
 inputTask.setAttribute('type', 'text');
 inputTask.setAttribute('placeholder', 'Enter a task');
 inputContainer.appendChild(inputTask);
 
-const AddButton = new Image(45, 45);
-AddButton.src = './Icons/add_task_icon.png';
-AddButton.setAttribute('id', 'add-task-button');
-inputContainer.appendChild(AddButton);
+const addButton = document.createElement('button');
+addButton.setAttribute('type', 'submit');
+addButton.setAttribute('id', 'add-task-button');
+
+const addButtonIcon = document.createElement('i');
+addButtonIcon.setAttribute('class', 'fas fa-plus-circle')
+addButton.appendChild(addButtonIcon);
+inputContainer.appendChild(addButton);
 
 // Task-Container
 const taskContainer = document.createElement('div');
-taskContainer.setAttribute('id', 'task-container');
+taskContainer.setAttribute('class', 'task-container');
+let list = document.createElement('ul');
+const clearAll = document.createElement('p');
+clearAll.setAttribute('id', 'clear-all');
+clearAll.textContent = 'Clear All';
+
+taskContainer.appendChild(list);
+taskContainer.appendChild(clearAll);
 main.appendChild(taskContainer);
+
 
 // Aktiva & Färdiga tasks counter 
 const counterContainer = document.createElement('div');
@@ -66,77 +78,104 @@ class Tasks {
     }
 }
 
+// Buttons 
+
+inputContainer.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if(inputTask.value != '') {
+        createNewTask(inputTask.value);   
+    }
+    if (inputTask.value == '') {
+        alert('Enter a task');
+    }
+    inputTask.value = '';
+})
+
+list.addEventListener('click', (e) => {
+    handleClickCheckDeleteName(e);
+})
+
+function handleClickCheckDeleteName(e) {
+    if (e.target.name == 'checkButton') {
+        checkTodo(e)
+    }
+    if (e.target.name == 'deleteButton') {
+        deleteTodo(e); 
+    }
+}
+
+clearAll.addEventListener('click', () => {
+    list.textContent = ' ';
+    taskActive = 0;
+    taskDone = 0;
+    todoList = [];
+    updateCounter();
+})
+
 // Functions
 
-function createNewTask() {
-        if (inputTask.value === '') {
-            alert('Please enter a task');
-        } else {
-            const newTask = new Tasks(inputTask.value)
+function checkTodo(e) {
+    let listNode = e.target.parentNode;
+    if (listNode.classList.contains('active')) {
+        e.target.src = './Icons/done_task_icon.png';
+        listNode.setAttribute('class', 'tasks done');
+        taskDone++;
+        taskActive--;
+
+    } else if (listNode.classList.contains('done')) {
+        e.target.src = './Icons/active_task_icon.png';
+        listNode.setAttribute('class', 'tasks active');
+        taskDone--;
+        taskActive++;
+    }
+    updateCounter();
+}
+
+function deleteTodo(e) {
+    let listNode = e.target.parentNode;
+    if (listNode.classList.contains('active')) {
+        taskActive--;
+    } else if (listNode.classList.contains('done')) {
+        taskDone--;
+    }
+    listNode.remove();
+    updateCounter();
+}
+
+function updateCounter() {
+    activeTasks.textContent = `Active: ${taskActive}`;
+    doneTasks.textContent = `Finished: ${taskDone}`;
+}
+
+ function createNewTask(todo) {
+            const newTask = new Tasks(todo)
             todoList.push(newTask);
             taskActive++;
             activeTasks.textContent = `Active: ${taskActive}`;
     
-            const task = document.createElement('div');
-            task.setAttribute('class', 'tasks');
-            taskContainer.appendChild(task);
+            const task = document.createElement('li');
+            task.setAttribute('class', 'tasks active');
+            list.appendChild(task);
     
             const taskCheck = new Image(20, 20);
             taskCheck.src = './Icons/active_task_icon.png';
             taskCheck.setAttribute('class', 'task-check active')
+            taskCheck.setAttribute('name', 'checkButton');
     
             const taskName = document.createElement('p');
             taskName.setAttribute('id', 'task-name');
-            taskName.textContent = inputTask.value;
+            taskName.textContent = todo;
     
             const deleteTask = new Image(20, 20);
             deleteTask.src = './Icons/trash_icon.png';
             deleteTask.setAttribute('class', 'delete-task active');
+            deleteTask.setAttribute('name', 'deleteButton');
             
     
             task.appendChild(taskCheck);
             task.appendChild(taskName);
             task.appendChild(deleteTask);
 
-            taskCheck.addEventListener('click', (e) => {
-                if(e.currentTarget.classList.contains('active')) {
-                    taskCheck.setAttribute('class', 'task-check done');
-                    deleteTask.setAttribute('class', 'delete-task done');
-                    taskCheck.src = './Icons/done_task_icon.png';
-                    taskDone++;
-                    taskActive--;
-                    doneTasks.textContent = `Finished: ${taskDone}`
-                    activeTasks.textContent = `Active: ${taskActive}`;
-                } else if (e.currentTarget.classList.contains('done')) {
-                    taskCheck.setAttribute('class', 'task-check active');
-                    deleteTask.setAttribute('class', 'delete-task active');
-                    taskCheck.src = './Icons/active_task_icon.png';
-                    taskDone--;
-                    taskActive++;
-                    doneTasks.textContent = `Finished: ${taskDone}`
-                    activeTasks.textContent = `Active: ${taskActive}`;
-                }
-                
-            });
-            deleteTask.addEventListener('click', (e) => {
-                if(e.currentTarget.classList.contains('active')) {
-                    taskActive--;
-                    activeTasks.textContent = `Active: ${taskActive}`;
-                } else if(e.currentTarget.classList.contains('done')){
-                    taskDone--;
-                    doneTasks.textContent = `Finished: ${taskDone}`
-                }
-                e.currentTarget.parentNode.remove();
-                });
+            
             
         }
-    }
-
-
-
-// Buttons **
-
-AddButton.addEventListener('click', () => {
-    createNewTask();
-    document.querySelector('#input-task').value = '';
-})
